69386361ff1b297944350752216db9647a65f665
nure-Voronin <dmytro.voronin@nure.ua>
2024-01-30 21:23:49 +0100

    Merge step8. Adde Hive Section List

--- KatlaSportNg/src/app/hive-management/services/hive.service.ts
+++ KatlaSportNg/src/app/hive-management/services/hive.service.ts
@@ -1,18 +1,18 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { environment } from 'environments/environment';
-import { Observable } from 'rxjs';
-import { Hive } from '../models/hive';
-import { HiveListItem } from '../models/hive-list-item';
-import { HiveSectionListItem } from '../models/hive-section-list-item';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { environment } from "environments/environment";
+import { Observable } from "rxjs";
+import { Hive } from "../models/hive";
+import { HiveListItem } from "../models/hive-list-item";
+import { HiveSectionListItem } from "../models/hive-section-list-item";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class HiveService {
-  private url = environment.apiUrl + 'api/hives/';
+  private url = environment.apiUrl + "api/hives/";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getHives(): Observable<Array<HiveListItem>> {
     return this.http.get<Array<HiveListItem>>(this.url);
@@ -23,7 +23,9 @@ export class HiveService {
   }
 
   getHiveSections(hiveId: number): Observable<Array<HiveSectionListItem>> {
-    return null;
+    return this.http.get<Array<HiveSectionListItem>>(
+      `${this.url}${hiveId}/sections`
+    );
   }
 
   addHive(hive: Hive): Observable<Hive> {
